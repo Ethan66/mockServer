749f5a30601aa6b9b7c92e6ec05d50216ba314bf
Ethan66 <2064281269@qq.com>
2017-07-19 17:32:57 +0800

    ajax demo

---
+++ loadmore/router.js
@@ -0,0 +1,15 @@
+
+app.get('/content', function(req, res) {
+	//req.query 可以获取请求参数
+	var index = req.query.index;
+	var num=req.query.num;
+	data=[];
+	for(var i=0;i<num;i++){
+		data.push("内容"+(parseInt(index)+i+1));
+	}
+	setTimeout(function(){
+        res.send(data);
+	},2000);
+
+});
+
