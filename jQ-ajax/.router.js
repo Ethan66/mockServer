db45803590af241265a0dd33d9d184b2e2ddf34c
Ethan66 <2064281269@qq.com>
2017-07-27 16:58:33 +0800

    用jQuey的ajax实现加载更多

---
+++ jQ-ajax/.router.js
@@ -0,0 +1,18 @@
+function setRouter(app){ 
+ var router = app; 
+
+app.get('/content', function(req, res) {
+    //req.query 可以获取请求参数
+    var index = req.query.index;
+    var num=req.query.num;
+    data=[];
+    for(var i=0;i<num;i++){
+        data.push("内容"+(parseInt(index)+i+1));
+    }
+    setTimeout(function(){
+        res.send(data);
+    },2000);
+
+});
+}
+ module.exports.setRouter = setRouter
